05fcb64c3385dd0ba02f5043b0cf4bce495bc088
Tiihott <48@teragrep.com>
2024-11-13 15:34:07 +0200

    Added refactored NewCommonConfiguration.java and test that use cnf_01.

---
+++ src/main/java/com/teragrep/cfe_39/configuration/NewCommonConfiguration.java
@@ -0,0 +1,205 @@
+/*
+ * HDFS Data Ingestion for PTH_06 use CFE-39
+ * Copyright (C) 2021-2024 Suomen Kanuuna Oy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ *
+ * Additional permission under GNU Affero General Public License version 3
+ * section 7
+ *
+ * If you modify this Program, or any covered work, by linking or combining it
+ * with other code, such other code is not for that reason alone subject to any
+ * of the requirements of the GNU Affero GPL version 3 as long as this Program
+ * is the same Program as licensed from Suomen Kanuuna Oy without any additional
+ * modifications.
+ *
+ * Supplemented terms under GNU Affero General Public License version 3
+ * section 7
+ *
+ * Origin of the software must be attributed to Suomen Kanuuna Oy. Any modified
+ * versions must be marked as "Modified version of" The Program.
+ *
+ * Names of the licensors and authors may not be used for publicity purposes.
+ *
+ * No rights are granted for use of trade names, trademarks, or service marks
+ * which are in The Program if any.
+ *
+ * Licensee must indemnify licensors and authors for any liability that these
+ * contractual assumptions impose on licensors and authors.
+ *
+ * To the extent this program is licensed as part of the Commercial versions of
+ * Teragrep, the applicable Commercial License may apply to this file if you as
+ * a licensee so wish it.
+ */
+package com.teragrep.cfe_39.configuration;
+
+import org.apache.logging.log4j.core.config.ConfigurationException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+
+public final class NewCommonConfiguration {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(NewCommonConfiguration.class);
+
+    private final Map<String, String> config;
+
+    public NewCommonConfiguration(Map<String, String> map) {
+        this.config = map;
+    }
+
+    // printers for configuration file paths.
+
+    public String egressConfigurationFile() {
+        return config
+                .getOrDefault("egress.configurationFile", System.getProperty("user.dir") + "/rpm/resources/egress.properties");
+    }
+
+    public String ingressConfigurationFile() {
+        return config
+                .getOrDefault("ingress.configurationFile", System.getProperty("user.dir") + "/rpm/resources/ingress.properties");
+    }
+
+    public String log4j2ConfigurationFile() {
+        return config
+                .getOrDefault("log4j2.configurationFile", System.getProperty("user.dir") + "/rpm/resources/log4j2.properties");
+    }
+
+    // printers for the configuration parameters.
+
+    public String queueTopicPattern() {
+        return config.getOrDefault("queueTopicPattern", ".*");
+    }
+
+    public int numOfConsumers() {
+        final String numString = config.get("numOfConsumers");
+        if (numString == null) {
+            throw new ConfigurationException("Configuration error. <numOfConsumers> must be set.");
+        }
+        else {
+            final int numOfConsumers;
+            try {
+                numOfConsumers = Integer.parseInt(numString);
+            }
+            catch (NumberFormatException e) {
+                LOGGER.error("Configuration error. Invalid value for <numOfConsumers>: <{}>", e.getMessage());
+                throw new RuntimeException(e);
+            }
+            if (numOfConsumers <= 0) {
+                throw new ConfigurationException("Configuration error. <numOfConsumers> must be a positive integer.");
+            }
+            else {
+                return numOfConsumers;
+            }
+        }
+    }
+
+    public String queueDirectory() {
+        return config.getOrDefault("queueDirectory", System.getProperty("user.dir") + "/rpm/resources/queue");
+    }
+
+    public long maximumFileSize() {
+        final String numString = config.get("maximumFileSize");
+        if (numString == null) {
+            throw new ConfigurationException("Configuration error. <maximumFileSize> must be set.");
+        }
+        else {
+            final long maximumFileSize;
+            try {
+                maximumFileSize = Long.parseLong(numString);
+            }
+            catch (NumberFormatException e) {
+                LOGGER.error("Configuration error. Invalid value for <maximumFileSize>: <{}>", e.getMessage());
+                throw new RuntimeException(e);
+            }
+            if (maximumFileSize <= 0) {
+                throw new ConfigurationException(
+                        "Configuration error. <maximumFileSize> must be a positive long value."
+                );
+            }
+            else {
+                return maximumFileSize;
+            }
+        }
+    }
+
+    public boolean skipNonRFC5424Records() {
+        final String skipString = config.get("skipNonRFC5424Records");
+        if (skipString == null) {
+            throw new ConfigurationException("Configuration error. <skipNonRFC5424Records> must be set.");
+        }
+        else {
+            return Boolean.parseBoolean(skipString);
+        }
+    }
+
+    public boolean skipEmptyRFC5424Records() {
+        final String skipString = config.get("skipEmptyRFC5424Records");
+        if (skipString == null) {
+            throw new ConfigurationException("Configuration error. <skipEmptyRFC5424Records> must be set.");
+        }
+        else {
+            return Boolean.parseBoolean(skipString);
+        }
+    }
+
+    public long pruneOffset() {
+        final String pruneString = config.get("pruneOffset");
+        if (pruneString == null) {
+            throw new ConfigurationException("Configuration error. <pruneOffset> must be set.");
+        }
+        else {
+            final long pruneOffset;
+            try {
+                pruneOffset = Long.parseLong(pruneString);
+            }
+            catch (NumberFormatException e) {
+                throw new RuntimeException(e);
+            }
+            if (pruneOffset <= 0) {
+                throw new ConfigurationException("Configuration error. <pruneOffset> must be a positive long value.");
+            }
+            else {
+                return pruneOffset;
+            }
+        }
+    }
+
+    public long consumerTimeout() {
+        final String consumerTimeoutString = config.get("consumerTimeout");
+        if (consumerTimeoutString == null) {
+            throw new ConfigurationException("Configuration error. <consumerTimeout> must be set.");
+        }
+        else {
+            final long consumerTimeout;
+            try {
+                consumerTimeout = Long.parseLong(consumerTimeoutString);
+            }
+            catch (NumberFormatException e) {
+                throw new RuntimeException(e);
+            }
+            if (consumerTimeout <= 0) {
+                throw new ConfigurationException(
+                        "Configuration error. <consumerTimeout> must be a positive long value."
+                );
+            }
+            else {
+                return consumerTimeout;
+            }
+        }
+    }
+
+}
